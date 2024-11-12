2663621bf276fa00db86d5ebdad1ef30f6207445
benjamin wiese <benjamin060101@gmail.com>
2024-11-12 16:13:46 +0100

    css

--- Models/MenuItem.cs
+++ Models/MenuItem.cs
@@ -2,10 +2,10 @@
 {
     public record MenuItemDTO
     {
-        public string Dag { get; set; }
+        public string? Dag { get; set; }
 
-        public string Varmret { get; set; }
+        public string? Varmret { get; set; }
 
-        public string Koldret { get; set; }
+        public string? Koldret { get; set; }
     }
 }
